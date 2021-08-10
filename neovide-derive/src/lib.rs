9788489b59b4b79ec90c856793c85b201eee7059
Keith Simmons <keith@the-simmons.net>
2021-08-09 23:50:17 -0700

    Prepare to use attributes

--- neovide-derive/src/lib.rs
+++ neovide-derive/src/lib.rs
@@ -4,18 +4,17 @@ use quote::quote;
 use syn::{parse_macro_input, Attribute, Data, DataStruct, DeriveInput, Error, Ident, Lit, Meta, Field, MetaNameValue};
 
 enum SettingType {
-    VimGlobal,
-    VimOption,
-    Environment,
-    CommandLineArgument,
+    Variable,
+    Option,
 }
 
 struct SettingData {
-    settingType: SettingType,
-    name: String,
+    setting_type: SettingType,
+    field_name: Ident,
+    vim_name: String,
 }
 
-#[proc_macro_derive(SettingGroup, attributes(setting_prefix, name, opt, env, cli))]
+#[proc_macro_derive(SettingGroup, attributes(setting_prefix, name, opt))]
 pub fn derive_setting_group(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
     let prefix = setting_prefix(input.attrs.as_ref())
@@ -31,66 +30,71 @@ pub fn derive_setting_group(item: TokenStream) -> TokenStream {
 
 fn derive_register_function(struct_name: Ident, prefix: String, data: &DataStruct) -> TokenStream {
     let fragments = data.fields.iter().map(|field| {
-        if let Some(field_name) = field.ident.as_ref() {
-            match parse_setting_type(field) {
-                Ok(SettingType::Default) => {
-                    let vim_setting_name = format!("{}{}", prefix, field_name);
-                    build_variable_fragments(&vim_setting_name, field_name, &struct_name)
-                },
-                Ok(SettingType::Global(vim_setting_name)) => build_variable_fragments(&vim_setting_name, field_name, &struct_name),
-                Err(error) => error.to_compile_error().into(),
-            }
-        } else {
-            Error::new_spanned(field.colon_token, "Expected named struct fields").to_compile_error().into()
+        match parse_setting_data(field, prefix.clone()) {
+            Ok(setting_data) => build_variable_fragments(setting_data, &struct_name),
+            Err(error) => error.to_compile_error().into()
         }
     });
 
     TokenStream::from(quote! {
         impl #struct_name {
             pub fn register() {
-                let s: Self = Default::default();
-                crate::settings::SETTINGS.set_global(&s);
+                let settings_struct: Self = Default::default();
+                crate::settings::SETTINGS.set_global(&settings_struct);
                 #(#fragments)*
             }
         }
     })
 }
 
-fn parse_setting_data(field: &Field) -> Result<SettingData, Error> {
+fn parse_setting_data(field: &Field, prefix: String) -> Result<SettingData, Error> {
     if field.attrs.len() > 1 {
         return Err(Error::new_spanned(field, "Field has multiple attributes"));
     }
 
-    if let Some(attribute) = field.attrs.first() {
-        if let Ok(Meta::NameValue(MetaNameValue { lit: Lit::Str(name), .. })) = attribute.parse_meta() {
-            if attribute.path.is_ident("global") {
-                Ok(SettingType::Global(name.value()))
+    if let Some(field_name) = field.ident.as_ref() {
+        if let Some(attribute) = field.attrs.first() {
+            if let Ok(Meta::NameValue(MetaNameValue { lit: Lit::Str(name), .. })) = attribute.parse_meta() {
+                if attribute.path.is_ident("opt") {
+                    Ok(SettingData {
+                        setting_type: SettingType::Option,
+                        field_name: field_name.clone(),
+                        vim_name: name.value(),
+                    })
+                } else {
+                    Err(Error::new_spanned(attribute, format!("Field attribute with path {:?} not recognized", attribute.path.get_ident())))
+                }
             } else {
-                Err(Error::new_spanned(attribute, format!("Field attribute with path {:?} not recognized", attribute.path)))
+                Err(Error::new_spanned(attribute, "Field attributes on SettingGroup must be name values"))
             }
         } else {
-            Err(Error::new_spanned(attribute, "Field attributes on SettingGroup must be name values"))
+            let vim_name = format!("{}{}", prefix, field_name);
+            Ok(SettingData {
+                setting_type: SettingType::Variable,
+                field_name: field_name.clone(),
+                vim_name,
+            })
         }
     } else {
-        Ok(SettingType::Default)
+        Err(Error::new_spanned(field.colon_token, "Expected named struct fields"))
     }
 }
 
-fn build_variable_fragments(vim_setting_name: &str, field_name: &Ident, struct_name: &Ident) -> TokenStream2 {
+fn build_variable_fragments(SettingData { field_name, vim_name, .. }: SettingData, struct_name: &Ident) -> TokenStream2 {
     let output_stream = quote! {{
         fn update_func(value: rmpv::Value) {
-            let mut s = crate::settings::SETTINGS.get::<#struct_name>();
-            s.#field_name.from_value(value);
-            crate::settings::SETTINGS.set(&s);
+            let mut setting_struct = crate::settings::SETTINGS.get_global::<#struct_name>();
+            setting_struct.#field_name.from_value(value);
+            crate::settings::SETTINGS.set(&setting_struct);
         }
 
         fn reader_func() -> rmpv::Value {
-            let s = crate::settings::SETTINGS.get::<#struct_name>();
-            s.#field_name.into()
+            let setting_struct = crate::settings::SETTINGS.get_global::<#struct_name>();
+            setting_struct.#field_name.into()
         }
 
         crate::settings::SETTINGS.set_setting_handlers(
-            #vim_setting_name,
+            #vim_name,
             update_func,
             reader_func
         );
